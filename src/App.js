700f11256ff77fe6b369cb62bb5f6a0a9140160a
simotae14 <startae14@gmail.com>
2019-09-11 23:57:24 +0200

    add react-router-dom to app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatPage = () => (
+  <div>
+    HAT PAGE
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route
+          exact
+          path='/'
+          component={HomePage}
+        />
+        <Route
+          path='/hats'
+          component={HatPage}
+        />
+      </Switch>
+
     </div>
   );
 }
