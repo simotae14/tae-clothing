56dd3ca0e0393231c569a8dada142a3caaaaf0e8
Simone Taeggi <startae14@gmail.com>
2019-09-14 20:16:06 +0200

    Adding the Custom Button inside the Sign in

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './sign-in.styles.scss';
 import FormInput from './../form-input/form-input.component';
+import CustomButton from './../custom-button/custom-button.component';
 
 class SignIn extends Component {
   constructor(props) {
@@ -34,10 +35,12 @@ class SignIn extends Component {
         <h2>I already have an account</h2>
         <span>Sign in with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <FormInput handleChange={this.handleChange} label='email' name='email' type='email' value={this.state.email} required />
-          <FormInput handleChange={this.handleChange} label='password' name='password' type='password' value={this.state.password} required />
+          <FormInput handleChange={this.handleChange} label='Email' name='email' type='email' value={this.state.email} required />
+          <FormInput handleChange={this.handleChange} label='Password' name='password' type='password' value={this.state.password} required />
           
-          <input type='submit' value='Submit Form' />
+          <CustomButton type='submit'>
+            Sign in
+          </CustomButton>
         </form>
       </div>
     )
