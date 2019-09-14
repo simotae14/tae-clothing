d715067b77fb588d3b20af93d5bba7bd7af55919
Simone Taeggi <startae14@gmail.com>
2019-09-14 17:50:39 +0200

    add the signinsignup page and the signin component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 function App() {
   return (
@@ -20,8 +21,11 @@ function App() {
           path='/shop'
           component={ShopPage}
         />
+        <Route
+          path='/signin'
+          component={SignInAndSignUpPage}
+        />
       </Switch>
-
     </div>
   );
 }
