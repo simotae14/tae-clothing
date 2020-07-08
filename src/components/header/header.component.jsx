68a514fd6f3b1c3fa94adf191e1b28e292486092
Simone <startae14@gmail.com>
2020-07-09 01:45:45 +0200

    aimplement cart dropdown component

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,6 +9,7 @@ import './header.styles.scss';
 import { auth } from './../../firebase/firebase.utils';
 
 import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 const Header = ({ currentUser }) => (
   <div className='header'>
@@ -27,6 +28,7 @@ const Header = ({ currentUser }) => (
       }
       <CartIcon />
     </div>
+    <CartDropdown />
   </div>
 );
 
