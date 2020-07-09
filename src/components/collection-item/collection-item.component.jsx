9c383da30f241ea5d3cad49e16a9cef6d260b4a7
Simone <startae14@gmail.com>
2020-07-09 23:49:14 +0200

    dispatch the add item action  when clicking on the button and update the cart items state

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
 
+import { addItem } from '../../redux/cart/cart.action';
+
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, imageUrl, price }) => {
+const CollectionItem = ({ item, addItem }) => {
+  const {
+    imageUrl,
+    name,
+    price
+  } = item;
   return (
     <div className='collection-item'>
       <div 
@@ -17,9 +25,13 @@ const CollectionItem = ({ id, name, imageUrl, price }) => {
         <div className='name'>{name}</div>
         <div className='price'>{price}</div>    
       </div>
-      <CustomButton inverted> Add to cart </CustomButton>
+      <CustomButton onClick={() => addItem(item)} inverted> Add to cart </CustomButton>
     </div>
   );
 };
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+  addItem: (item) => dispatch(addItem(item))
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
