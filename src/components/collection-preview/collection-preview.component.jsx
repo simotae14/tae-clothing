1046a9646f2ce30dfbf731ca46e7c7b6a4d827c6
Simone <startae14@gmail.com>
2020-07-10 00:14:24 +0200

    create an utility to add the articles in the cart without repetitions but using a quantity pty

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import CollectionItem from './../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => {
--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -1,5 +1,8 @@
 import { CartActionTypes } from './cart.types';
 
+import { addItemToCart } from './cart.utils';
+
+
 // Define the initial State
 const INITIAL_STATE = {
   hidden: true,
@@ -16,7 +19,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItems: [...state.cartItems, action.payload]
+        cartItems: addItemToCart(state.cartItems, action.payload)
       }
     default:
       return state;
