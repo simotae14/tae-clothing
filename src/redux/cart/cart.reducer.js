249f3462a84b97b4c412a1a5e974c8839b6fdb45
Simone <startae14@gmail.com>
2020-07-21 21:23:38 +0200

    add the remove item from cart action, its reducer and use it inside the checkout item component when clicking on the X button

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -22,6 +22,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         // call utils to add item
         cartItems: addItemToCart(state.cartItems, action.payload)
       }
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(cartItem => cartItem.id !== action.payload.id)
+      }
     default:
       return state;
   }
