7dd52a31e35e9bd397cdc4e3a6b8876898c6ff4f
Simone <startae14@gmail.com>
2020-07-23 23:15:34 +0200

    add selector to retrieve the collection regarding a dynamic page

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { selectCollection } from '../../redux/shop/shop.selectors';
+
 
 import CollectionItem from '../../components/collection-item/collection-item.component';
 
 import './collection.component.scss';
 
-const CollectionPage = ({ match }) => {
-  console.log(match.params.collectionId);
+const CollectionPage = ({ collection }) => {
+  console.log(collection);
   return (
     <div className='collection-page'>
       <h2>COLLECTION PAGE</h2>
@@ -13,4 +17,8 @@ const CollectionPage = ({ match }) => {
   );
 };
 
-export default CollectionPage;
+const mapStateToProps = (state, ownProps) => ({
+  collection: selectCollection(ownProps.match.params.collectionId)(state)
+});
+
+export default connect(mapStateToProps)(CollectionPage);
--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,5 +1,14 @@
 import { createSelector } from 'reselect';
 
+// ID MAP
+const COLLECTION_ID_MAP = {
+  hats: 1,
+  sneakers: 2,
+  jackets: 3,
+  womens: 4,
+  mens: 5 
+};
+
 // INPUT SELECTOR
 const selectShop = state => state.shop;
 
@@ -7,4 +16,10 @@ const selectShop = state => state.shop;
 export const selectShopCollections = createSelector(
   [selectShop],
   shop => shop.collections
+);
+
+// COLLECTION SELECTOR
+export const selectCollection = collectionUrlParam => createSelector(
+  [selectShopCollections],
+  collections => collections.find(collection => collection.id === COLLECTION_ID_MAP[collectionUrlParam])
 );
