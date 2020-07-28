57f004dc5e42fe7931a62e267ead056134ce9ade
Simone <startae14@gmail.com>
2020-07-28 23:04:07 +0200

    convert collection-preview in a styled component

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 
 import CollectionItem from './../collection-item/collection-item.component';
 
-import './collection-preview.styles.scss';
+import {
+  CollectionPreviewContainer,
+  PreviewContainer,
+  TitleContainer
+} from './collection-preview.styles';
 
 const CollectionPreview = ({ title, items }) => {
   return (
-    <div className='collection-preview'>
-      <h1 className='title'>{title.toUpperCase()}</h1>
-      <div className='preview'>
+    <CollectionPreviewContainer>
+      <TitleContainer>{title.toUpperCase()}</TitleContainer>
+      <PreviewContainer>
         {
           items
             .filter((item, index) => index < 4)
@@ -16,8 +20,8 @@ const CollectionPreview = ({ title, items }) => {
               <CollectionItem key={item.id} item={item} />
             ))
         }
-      </div>
-    </div>
+      </PreviewContainer>
+    </CollectionPreviewContainer>
   );
 };
 
