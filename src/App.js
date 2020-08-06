7b12653839fbf2223fc7e6968711124cd16049db
Simone <startae14@gmail.com>
2020-08-06 23:38:29 +0200

    cremove call to add collections in firestore

--- src/App.js
+++ src/App.js
@@ -15,16 +15,15 @@ import { createStructuredSelector } from 'reselect';
 
 import { setCurrentUser } from './redux/user/user.action';
 import { selectCurrentUser } from './redux/user/user.selectors';
-import { selectCollectionForPreview } from './redux/shop/shop.selectors';
 
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
   // function to unsuscribe the user
   unsuscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
     // get the function to call to unsuscribe
     this.unsuscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
@@ -42,7 +41,6 @@ class App extends React.Component {
         // if the user is not logged with update the current user
         // with null
         setCurrentUser(userAuth);
-        addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })));
       }
     });
   }
@@ -82,8 +80,7 @@ class App extends React.Component {
 }
 
 const mapStateToProps = createStructuredSelector({
-  currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionForPreview
+  currentUser: selectCurrentUser
 });
 
 const mapDispatchToProps = dispatch => ({
