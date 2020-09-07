6222abd830c9e424bab9e23a31d6182cc3cd8c1d
Simone <startae14@gmail.com>
2020-09-07 23:41:30 +0200

    use WithSpinner HOC in our routes

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
+import WithSpinner from '../../components/with-spinner/with-spinner.component';
+
 import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 import CollectionPage from '../collection/collection.component';
 
@@ -11,7 +13,14 @@ import {
   updateCollections
 } from '../../redux/shop/shop.action';
 
+// use the hoc
+const CollectionsOverviewWithSpinner = WithSpinner(CollectionsOverview);
+const CollectionPageWithSpinner = WithSpinner(CollectionPage);
+
 class ShopPage extends Component {
+  state = {
+    loading: true
+  };
   // function to unsuscribe the snapshot
   unsuscribeFromSnapshot = null;
   componentDidMount() {
@@ -26,16 +35,23 @@ class ShopPage extends Component {
     collectionRef.onSnapshot(async snapshot => {
       const collectionsMap = convertCollectionSnapshotToMap(snapshot);
       updateCollections(collectionsMap);
+      // set loading to false
+      this.setState({
+        loading: false
+      });
     });
   }
   render() {
     const {
       match
     } = this.props;
+    const {
+      loading
+    } =  this.state;
     return (
       <div className='shop-page'>
-      <Route exact path={`${match.path}`} component={CollectionsOverview} />  
-      <Route path={`${match.path}/:collectionId`} component={CollectionPage} />
+      <Route exact path={`${match.path}`} render={props => <CollectionsOverviewWithSpinner isLoading={loading} {...props} />} />  
+      <Route path={`${match.path}/:collectionId`} render={props => <CollectionPageWithSpinner isLoading={loading} {...props} />} />
     </div> 
     );
   }
