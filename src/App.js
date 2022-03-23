4223b13ca6a80cdec312a2024cb7d97fe1d31939
Simone <startae14@gmail.com>
2022-03-24 00:27:49 +0100

    refactor the code to create a Category component

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import CategoryItem from './components/category-item/category-item.component';
 import './categories.styles.scss';
 
 const App = () => {
@@ -31,19 +32,8 @@ const App = () => {
   return (
     <div className='categories-container'>
       {
-        categories.map(({ id, title, imageUrl }) => (
-          <div key={id} className='category-container'>
-            <div
-              className='background-image'
-              style={{
-                backgroundImage: `url(${imageUrl})`
-              }}
-            />
-            <div className='category-body-container'>
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
+        categories.map((category) => (
+          <CategoryItem key={category.id} category={category} />
         ))
       }
     </div>
