b2263b730e2872befe07fed76c70c855c8cf5996
Simone <startae14@gmail.com>
2022-03-24 12:33:38 +0100

    define a Shared Navigation between pages using the Outlet component of react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const Navigation = () => {
+  return (
+    <div>
+      <div>
+        <h1>
+          I am the navigation bar
+        </h1>
+      </div>
+      <Outlet />
+    </div>
+  );
+}
+
 const Shop = () => {
-  return <div>I am the shop component</div>
+  return <h1>I am the shop component</h1>
 }
 
 const App = () => {
   return (
     <Routes>
       <Route
-        path='/home'
-        element={<Home />}
+        path='/'
+        element={<Navigation />}
       >
         <Route
-          path='shop' // /home/shop
+          index
+          element={<Home />}
+        />
+        <Route
+          path='shop'
           element={<Shop />}
         />
       </Route>
