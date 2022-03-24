449bf0a9de3943d696e3a317d29a2603a7503298
Simone <startae14@gmail.com>
2022-03-25 00:40:49 +0100

    install firebase and create the navigation and the page for the sign in

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
+import SignIn from './routes/sign-in/sign-in.component';
 
 const Shop = () => {
   return <h1>I am the shop component</h1>
@@ -21,6 +22,10 @@ const App = () => {
         <Route
           path='shop'
           element={<Shop />}
+        />
+        <Route
+          path='sign-in'
+          element={<SignIn />}
         />
       </Route>
     </Routes>
