43801e55eb4df2dc50dfff31a19a78cae4b4834a
Simone <startae14@gmail.com>
2022-03-24 12:19:11 +0100

    use the Outlet component for the nested routes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 
 import Home from './routes/home/home.component';
 
+const Shop = () => {
+  return <div>I am the shop component</div>
+}
+
 const App = () => {
   return (
     <Routes>
       <Route
         path='/home'
         element={<Home />}
-      />
+      >
+        <Route
+          path='shop' // /home/shop
+          element={<Shop />}
+        />
+      </Route>
     </Routes>
   );
 }
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,3 +1,5 @@
+import { Outlet } from 'react-router-dom';
+
 import Directory from '../../components/directory/directory.component';
 
 const Home = () => {
@@ -28,7 +30,12 @@ const Home = () => {
       "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
     }
   ];
-  return <Directory categories={categories} />;
+  return (
+    <div>
+      <Directory categories={categories} />
+      <Outlet />
+    </div>
+  );
 }
 
 export default Home;
