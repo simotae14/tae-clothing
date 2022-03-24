753da6e54865b88b93b24654738da060607b7282
Simone <startae14@gmail.com>
2022-03-24 17:11:35 +0100

    Add some style to the navigation bar

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,13 +1,15 @@
 import { Outlet, Link } from 'react-router-dom';
 
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
   return (
     <>
       <div className='navigation'>
         <Link className='logo-container' to='/'>
-          <div>
-            Logo
-          </div>
+          <CrownLogo className='logo' />
         </Link>
         <div className='nav-links-container'>
           <Link className='nav-link' to='/shop'>
