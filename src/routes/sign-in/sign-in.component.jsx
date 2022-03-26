d5857adee019bf820a49da0dc802010b18b5bab0
Simone <startae14@gmail.com>
2022-03-27 00:25:44 +0100

    Define the Sign up Form with its State and onChange method

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,21 +1,11 @@
-import { useEffect } from 'react';
-import { getRedirectResult } from 'firebase/auth';
-
 import  {
-  auth,
   signInWithGooglePopup,
-  signInWithGoogleRedirect,
   createUserDocumentFromAuth,
 } from '../../utils/firebase/firebase.utils';
 
-const SignIn = () => {
-  useEffect(async () => {
-    const response = await getRedirectResult(auth);
-    if (response) {
-      const userDocRef = await createUserDocumentFromAuth(response.user);
-    }
-  }, []);
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component';
 
+const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
@@ -27,9 +17,7 @@ const SignIn = () => {
       <button onClick={logGoogleUser}>
         Sign in with Google Popup
       </button>
-      <button onClick={signInWithGoogleRedirect}>
-        Sign in with Google Redirect
-      </button>
+      <SignUpForm />
     </div>
   );
 };
