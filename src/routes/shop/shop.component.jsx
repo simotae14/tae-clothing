54a8e1b7ecad04afad22c648c8736cc81bea2e0c
Simone <startae14@gmail.com>
2022-03-27 17:14:28 +0200

    Created the ProductCard component and the Products Grid

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -2,15 +2,17 @@ import { useContext } from 'react';
 
 import { ProductsContext } from '../../contexts/products.context';
 
+import ProductCard from '../../components/product-card/product-card.component';
+
+import './shop.styles.scss';
+
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
+    <div className='products-container'>
       {
-        products.map(({id, name}) => (
-          <div key={id}>
-            <h1>{name}</h1>
-          </div>
+        products.map(product => (
+          <ProductCard key={product.id} product={product} />
         ))
       }
     </div>
