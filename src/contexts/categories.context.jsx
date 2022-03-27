97ce017cb3c41897647b9693842d8d6dd8d08ae2
Simone <startae14@gmail.com>
2022-03-28 00:57:10 +0200

    retrieve all the categories in the shop component route

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -4,23 +4,23 @@ import {
   getCategoriesAndDocuments,
 } from '../utils/firebase/firebase.utils';
 
-export const ProductsContext = createContext({
-  products: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
-      console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     }
     getCategoriesMap();
   }, []);
-  const value = { products };
+  const value = { categoriesMap };
   return (
-    <ProductsContext.Provider value={value}>
+    <CategoriesContext.Provider value={value}>
       {children}
-    </ProductsContext.Provider>
+    </CategoriesContext.Provider>
   );
 };
