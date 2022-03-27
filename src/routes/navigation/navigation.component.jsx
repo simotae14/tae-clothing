70e9bbc585cd79e2ddb5212f17c3919f380bb372
Simone <startae14@gmail.com>
2022-03-27 17:34:25 +0200

    Created the Dropdwon Cart component with its style always visible

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react';
 import { Outlet, Link } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
+import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
 
 import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
 import {
@@ -45,6 +46,7 @@ const Navigation = () => {
           }
           <CartIcon />
         </div>
+        <CartDropdown />
       </div>
       <Outlet />
     </>
