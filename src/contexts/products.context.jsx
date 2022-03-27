3283d500ca8e8a85e823607695e370ca3bc61019
Simone <startae14@gmail.com>
2022-03-28 00:14:14 +0200

    remove the useEffect inside products context to avoid in every run the write on the DB

--- src/contexts/products.context.jsx
+++ src/contexts/products.context.jsx
@@ -1,10 +1,4 @@
-import { createContext, useState, useEffect } from 'react';
-
-import {
-  addCollectionAddDocuments,
-} from '../utils/firebase/firebase.utils';
-
-import SHOP_DATA from '../shop-data.js';
+import { createContext, useState } from 'react';
 
 export const ProductsContext = createContext({
   products: [],
@@ -12,9 +6,6 @@ export const ProductsContext = createContext({
 
 export const ProductsProvider = ({ children }) => {
   const [products, setProducts] = useState([]);
-  useEffect(() => {
-    addCollectionAddDocuments('categories', SHOP_DATA);
-  }, []);
   const value = { products };
   return (
     <ProductsContext.Provider value={value}>
