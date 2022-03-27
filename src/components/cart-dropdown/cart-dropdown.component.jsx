73d22c16740f4e620a1722713e2e1763967cf95d
Simone <startae14@gmail.com>
2022-03-27 19:58:03 +0200

    Create the Checkout route and add a navigate method to go to this page clicking on the button inside the Dropdown Cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,4 +1,5 @@
 import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import { CartContext } from '../../contexts/cart.context';
 
@@ -11,6 +12,11 @@ const CartDropdown = () => {
   const {
     cartItems,
   } = useContext(CartContext);
+
+  const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  }
   return (
     <div className='cart-dropdown-container'>
       <div className='cart-items'>
@@ -20,7 +26,7 @@ const CartDropdown = () => {
           ))
         }
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
     </div>
   );
 };
---
+++ src/routes/checkout/checkout.component.jsx
@@ -0,0 +1,11 @@
+import './checkout.styles.scss';
+
+const Checkout = () => {
+  return (
+    <div>
+      <h1>I am the checkout page</h1>
+    </div>
+  );
+};
+
+export default Checkout;
