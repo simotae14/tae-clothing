714731844293986c4e1da98059ffa4991511b25b
Simone <startae14@gmail.com>
2022-03-27 04:01:17 +0200

    handle all the possible errors in login, define the type of the button inside a form if it's not a submit, and give some style to the sign-in page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -37,9 +37,18 @@ const SignInForm = () => {
     event.preventDefault();
     try {
       const response = await signInAuthUserWithEmailAndPassword(email, password);
-      console.log(response);
       resetFormFields();
     } catch (error) {
+      switch(error.code) {
+        case 'auth/wrong-password':
+          alert('Incorrect password for email');
+          break;
+        case 'auth/user-not-found':
+          alert('No user associated with this email');
+          break
+        default:
+          console.log(error);
+      }
     }
   }
 
@@ -78,7 +87,7 @@ const SignInForm = () => {
         />
         <div className='buttons-container'>
           <Button type='submit'>Sign In</Button>
-          <Button buttonType='google' onClick={signInWithGoogle}>Sign In With Google</Button>
+          <Button type='button' buttonType='google' onClick={signInWithGoogle}>Sign In With Google</Button>
         </div>
       </form>
     </div>
