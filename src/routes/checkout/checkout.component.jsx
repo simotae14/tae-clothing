51fdf8dc587049a5bbb7df151bfc27c754e891af
Simone <startae14@gmail.com>
2022-03-27 21:02:03 +0200

    Add the style to the Checkout page and create the Checkout item component with its style

--- src/routes/checkout/checkout.component.jsx
+++ src/routes/checkout/checkout.component.jsx
@@ -4,6 +4,8 @@ import {
   CartContext,
 } from '../../contexts/cart.context';
 
+import CheckoutItem from '../../components/checkout-item/checkout-item.component';
+
 import './checkout.styles.scss';
 
 const Checkout = () => {
@@ -13,29 +15,28 @@ const Checkout = () => {
     removeItemFromCart,
   } = useContext(CartContext);
   return (
-    <div>
-      <h1>I am the checkout page</h1>
-      <div>
-        {
-          cartItems.map((cartItem) => {
-            const {
-              id,
-              name,
-              quantity,
-            } = cartItem;
-            return (
-              <div key={id}>
-                <h2>{name}</h2>
-                <span>{quantity}</span>
-                <br />
-                <span onClick={() => removeItemFromCart(cartItem)}>decrement</span>
-                <br />
-                <span onClick={() => addItemToCart(cartItem)}>increment</span>
-              </div>
-            )
-          })
-        }
+    <div className='checkout-container'>
+      <div className='checkout-header'>
+        <div className='header-block'>
+          <span>Product</span>
+        </div>
+        <div className='header-block'>
+          <span>Description</span>
+        </div>
+        <div className='header-block'>
+          <span>Quantity</span>
+        </div>
+        <div className='header-block'>
+          <span>Price</span>
+        </div>
+        <div className='header-block'>
+          <span>Remove</span>
+        </div>
       </div>
+      {
+        cartItems.map((cartItem) => (<CheckoutItem key={cartItem.id} cartItem={cartItem} />))
+      }
+      <span className='total'>Total: 0</span>
     </div>
   );
 };
