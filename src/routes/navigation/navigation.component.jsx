3da67e723e7c6958c9d9ffd7f65cca1a3491d3f2
Simone <startae14@gmail.com>
2022-03-28 16:40:58 +0200

    Refactor the Navigation style using STYLED COMPONENTS

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,5 +1,5 @@
 import { useContext } from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 
 import CartIcon from '../../components/cart-icon/cart-icon.component';
 import CartDropdown from '../../components/cart-dropdown/cart-dropdown.component';
@@ -16,7 +16,12 @@ import {
   signOutUser,
 } from '../../utils/firebase/firebase.utils';
 
-import './navigation.styles.scss';
+import {
+  NavigationContainer,
+  LogoContainer,
+  NavLinks,
+  NavLink,
+} from './navigation.styles';
 
 const Navigation = () => {
   const {
@@ -29,34 +34,34 @@ const Navigation = () => {
 
   return (
     <>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
+      <NavigationContainer>
+        <LogoContainer to='/'>
           <CrownLogo className='logo' />
-        </Link>
-        <div className='nav-links-container'>
-          <Link className='nav-link' to='/shop'>
+        </LogoContainer>
+        <NavLinks>
+          <NavLink to='/shop'>
             SHOP
-          </Link>
+          </NavLink>
           {
             currentUser ? (
-              <span
+              <NavLink
+                as='span'
                 onClick={signOutUser}
-                className='nav-link'
               >
                 SIGN OUT
-              </span>
+              </NavLink>
             ) : (
-              <Link className='nav-link' to='/auth'>
+              <NavLink to='/auth'>
                 SIGN IN
-              </Link>
+              </NavLink>
             )
           }
           <CartIcon />
-        </div>
+        </NavLinks>
         {
           isCartOpen && <CartDropdown />
         }
-      </div>
+      </NavigationContainer>
       <Outlet />
     </>
   );
