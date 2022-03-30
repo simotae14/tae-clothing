2e97ebe2804569b1aa51ec7aa81267ce7da1879e
Simone <startae14@gmail.com>
2022-03-31 01:38:10 +0200

    configured and tested a payment using strapi and the Stateless Function defined

--- src/components/payment-form/payment-form.component.jsx
+++ src/components/payment-form/payment-form.component.jsx
@@ -1,4 +1,4 @@
-import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { CardElement, useStripe, useElements } from "@stripe/react-stripe-js";
 
 import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
@@ -10,14 +10,49 @@ import {
 const PaymentForm = () => {
   const stripe = useStripe();
   const elements = useElements();
-  const paymentHandler = async (event) => {
-    event.preventDefault();
-    // if there is no stripe or elements instance then exit
-    if (!stripe || !elements) return;
+  const paymentHandler = async (e) => {
+    e.preventDefault();
+    if (!stripe || !elements) {
+      return;
+    }
+
+    const response = await fetch("/.netlify/functions/create-payment-intent", {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ amount: 10000 })
+    }).then((res) => {
+      return res.json();
+    });;
+
+    const {
+      paymentIntent: {
+        client_secret,
+      }
+    } = response;
+    console.log(client_secret);
+    const paymentResult = await stripe.confirmCardPayment(client_secret, {
+      payment_method: {
+        card: elements.getElement(CardElement),
+        billing_details: {
+          name: 'Simotae'
+        }
+      }
+    });
+
+    // check payment
+    if (paymentResult.error) {
+      alert(paymentResult.error);
+    } else {
+      if (paymentResult.paymentIntent.status === 'succeeded') {
+        alert('Payment Successful');
+      }
+    }
   }
   return (
     <PaymentFormContainer>
-      <FormContainer>
+      <FormContainer onSubmit={paymentHandler}>
         <h2>Credit Card Payment: </h2>
         <CardElement />
         <Button buttonType={BUTTON_TYPE_CLASSES.inverted}>Pay now</Button>
