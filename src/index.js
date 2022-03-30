de414b0e7451252b69dfb4ef283616ffba62ba84
Simone <startae14@gmail.com>
2022-03-30 02:55:20 +0200

    use React-Persist to store our data in the local storage and have them in every session

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 import App from './App';
-import { store } from './store/store';
+import { store, persistor } from './store/store';
 
 import reportWebVitals from './reportWebVitals';
 
@@ -13,9 +14,11 @@ import './index.scss';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
