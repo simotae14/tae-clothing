2d4662055c41436d745b8ca9ba4f8cfa8ba10182
Simone <startae14@gmail.com>
2022-04-02 20:21:01 +0200

    add service Worker into your entry point

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import { PersistGate } from 'redux-persist/integration/react';
 import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
 
@@ -25,6 +26,11 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
